56024610dad46c117826a260091ed55411079f36
paul-ver <paul--verhoeven@hotmail.com>
2020-03-22 14:46:33 +0100

    revert to authorative server

--- server/networking.js
+++ server/networking.js
@@ -36,31 +36,15 @@ wss.on('connection', function connection(ws) {
 	broadcast(MSG.JOIN + "," + player.id, ws);
 
 	ws.on('message', (msg) => {
-		//log(msg);
 		var args = String(msg).split(",");
-		//log("RECEIVE:",args);
 
-		args.splice(1,0,""+player.id);
-		console.log(JSON.stringify(args));
-		broadcast(String(args),ws);
-
-		//Example of non-authorative server.
-		//This server will broadcast every incoming message of any player; to all other players.
-		//The players themselves will send their rotation/location.
-
-		//This makes the server code very simple.
-		//For example; to add bullets; only client-sided you'll have to implement that "message id 6 = bullet". And thus implement that the client will send "6" when shooting, and makes the otherplayer spawn a bullet when "6" is received.
-			//This will instantly work on the server; because the server doesn't care, and will forward it.
-			//You can however; add a 'firewall' or rate limiting on the server, so that if the "bullet" message is received from client 1, it will stop forwarding bullet messages of this client for x amount of time.
-			//Pushing this code as an example; I'm going to refactor the networking next iteration.
-
-		/*switch (parseInt(args[0])) {
-			case MSG.LOCATION: //location
+		switch (parseInt(args[0])) {
+			case MSG.LOCATION:
 				player.x = parseInt(args[1]);
 				player.y = parseInt(args[2]);
 				broadcast(MSG.LOCATION + "," + player.id + "," + args[1] + "," + args[2], ws);
 				break;
-			case MSG.RENAME: //set name
+			case MSG.RENAME:
 				broadcast(MSG.RENAME + "," + player.id + "," + args[1], ws);
 				player.name = args[1];
 				log(player.id + " name changed", player.name);
@@ -74,8 +58,10 @@ wss.on('connection', function connection(ws) {
 				break;
 			default:
 				console.log("UNKNOWN MSG:", msg);
+				args.splice(1,0,""+player.id);
+				broadcast(String(args),ws);
 				break;
-		}*/
+		}
 	});
 
 	ws.isAlive = true;
